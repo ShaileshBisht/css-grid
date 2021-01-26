f0606224eddac1be97d1a6f361d1b4771da75e76
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-26 23:13:05 +0530

    add animation to title

--- src/component/Top.js
+++ src/component/Top.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Top.css";
+import gsap from "gsap";
 
 function Top() {
+  const t1 = gsap.timeline();
+
+  useEffect(() => {
+    t1.from(".top_h1", {
+      duration: 3,
+      scale: 10,
+      ease: "elastic.out(1, 0.3)",
+      y: "-200px",
+      opacity: 0,
+    });
+  }, [t1]);
+
   return (
     <div className="top">
       <div className="top_title">
