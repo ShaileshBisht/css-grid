cbd45a3d4c51368daa03838aaa287d158c56bcb8
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-26 21:44:12 +0530

    add Top.js component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import CssGrid from "./component/CssGrid";
+import Top from "./component/Top";
 
 function App() {
   return (
     <div className="app">
+      <Top />
       <CssGrid />
     </div>
   );
